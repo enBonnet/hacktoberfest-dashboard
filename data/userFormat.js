f9d9d0fdab14bd4c32c992a727448280722b0b4f
Ender Bonnet <enbonnet@gmail.com>
2020-10-01 15:59:53 -0300

    actualizar funcion para transformar usuario en color

--- data/userFormat.js
+++ data/userFormat.js
@@ -1,16 +1,15 @@
 import usersStorage from "./usersStorage.json";
 
 const hashCode = (str) => {
-  var hash = 0;
-  for (var i = 0; i < str.length; i++) {
+  let hash = 10;
+  for (let i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
   return hash;
 };
 
 const intToRGB = (i) => {
-  var c = (i & 0x00ffffff).toString(16).toUpperCase();
-
+  let c = (i & 0x00ffffff).toString(16).toUpperCase();
   return "00000".substring(0, 6 - c.length) + c;
 };
 
